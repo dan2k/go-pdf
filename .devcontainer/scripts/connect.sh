dc228b47bbf3200142d7021a31350508e15f3671
dan2k <sangtong@gmail.com>
2022-05-20 12:54:43 +0000

    update config

--- .devcontainer/scripts/connect.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo cdgs$8133Tong|/openconnect/openconnect --protocol=gp extrapass.cdg.co.th --user=003459 --no-dtls -b
-
-sleep 5
-
-iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
-iptables -A FORWARD -i eth0 -j ACCEPT
-
-/bin/bash
