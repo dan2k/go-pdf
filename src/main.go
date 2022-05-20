75ed75ab93efd20e8a8357e446feef016852cdb1
dan2k <sangtong@gmail.com>
2022-05-20 13:28:46 +0000

    test

--- src/main.go
+++ src/main.go
@@ -3,9 +3,34 @@ package main
 import (
 	"fmt"
 	"www/src/router"
+	"log"
+    "github.com/PaloAltoNetworks/pango"
+	"github.com/go-ping/ping"
 )
 
 func main() {
+	var err error
+
+    c := &pango.Firewall{Client: pango.Client{
+        Hostname: "extrapass.cdg.co.th",
+        Username: "003459",
+        Password: "cdgs$8133Tongx",
+        Logging: pango.LogAction | pango.LogOp,
+    }}
+    if err = c.Initialize(); err != nil {
+        log.Printf("Failed to initialize client: %s", err)
+        return
+    }
+    log.Printf("Initialize ok")
+	p, err := ping.NewPinger("10.255.1.150")
+	if err != nil {
+		log.Printf("Cannot connect to 10.254.1.177: %s", err)
+	} else{
+		log.Printf("Ping complete... %v",p)
+	}
+	
+
+
 	fmt.Println("Wellcome to Golang")
 	fmt.Println("Start to Programming golang")
 	rt := router.Init()
