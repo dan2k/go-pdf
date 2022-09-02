32a4e014afa0afc42da01f5922a87304c6d19ac7
dan2k <sangtong@gmail.com>
2022-09-02 04:54:19 +0000

    set write log to file

--- main.go
+++ main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"fmt"
 	u "go-pdf/pdfGenerator"
-	"github.com/schollz/progressbar/v3"
-	"time"
-	"github.com/xuri/excelize/v2"
 	"log"
-    "os"
+	"os"
+	"time"
+
+	"github.com/schollz/progressbar/v3"
+	"github.com/xuri/excelize/v2"
 )
+
 var (
-    outfile, _ = os.Create("my.log") // update path for your needs
-    l      = log.New(outfile, "", 0)
+	outfile, _ = os.Create("my.log") // update path for your needs
+	l          = log.New(outfile, "", 0)
 )
+
 const (
-    DDMMYYYYhhmmss = "2006-01-02 15:04:05"
+	DDMMYYYYhhmmss = "2006-01-02 15:04:05"
 )
+
 func main() {
 
 	r := u.NewRequestPdf("")
@@ -48,80 +52,62 @@ func main() {
 		fmt.Println(err)
 	}
 
-	/*
-	bar := progressbar.NewOptions(
-		100, 
-		progressbar.OptionSetPredictTime(true),
-		progressbar.OptionShowElapsedTimeOnFinish(),
-		progressbar.OptionShowCount(),
-		progressbar.OptionSetDescription("กำลังประมวลผล..."),
-	)
 	
-	for i := 0; i < 100; i++ {
-		bar.Add(1)
-		time.Sleep(40 * time.Millisecond)
-	}
-	
-	fmt.Println("")
-	*/
 	//######################################################
-	var xlsFile ="test.xlsx"
-	fmt.Printf("Open %s \n",xlsFile)
+	var xlsFile = "test.xlsx"
+	fmt.Printf("Open %s \n", xlsFile)
+	l.Printf("%s Open %s \n",time.Now().UTC().Format(DDMMYYYYhhmmss),xlsFile)
 	f, err := excelize.OpenFile(xlsFile)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	if err != nil {
+		fmt.Println(err)
+		l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss),err)
+		return
+	}
 	fmt.Println("Open file complete!")
-    defer func() {
-        // Close the spreadsheet.
-        if err := f.Close(); err != nil {
-            fmt.Println(err)
-        }
-    }()
-    // Get value from cell by given worksheet name and axis.
-	
+	l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss),"Open file complete!")
+	defer func() {
+		// Close the spreadsheet.
+		if err := f.Close(); err != nil {
+			fmt.Println(err)
+			l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss),err)
+		}
+	}()
+	// Get value from cell by given worksheet name and axis.
+
 	rows, err := f.GetRows("Sheet1")
 
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-	fmt.Printf("Total %d rows \n",len(rows)-1)
+	if err != nil {
+		fmt.Println(err)
+		l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss),err)
+		return
+	}
+	fmt.Printf("Total %d rows \n", len(rows)-1)
+	l.Printf("%s Total %d rows \n",time.Now().UTC().Format(DDMMYYYYhhmmss), len(rows)-1)
 	bar := progressbar.NewOptions(
-		len(rows)-1, 
+		len(rows)-1,
 		progressbar.OptionEnableColorCodes(true),
 		progressbar.OptionSetPredictTime(true),
 		progressbar.OptionShowElapsedTimeOnFinish(),
 		progressbar.OptionShowCount(),
+		progressbar.OptionFullWidth(),
 		progressbar.OptionSetDescription("กำลังประมวลผล..."),
 		progressbar.OptionSetTheme(progressbar.Theme{
 			Saucer:        "[green]▓[reset]",
-        	SaucerHead:    "[green]▶[reset]",
-			SaucerPadding: "",
+			SaucerHead:    "[green]▶[reset]",
+			SaucerPadding: " ",
 			BarStart:      "[",
 			BarEnd:        "]",
 		}),
 	)
-	/*
-    for _, row := range rows {
-        for _, colCell := range row {
-            fmt.Print(colCell, "\t")
-        }
-        fmt.Println()
-    }
-	*/
-	
 	for i := 1; i < len(rows); i++ {
 		r.GeneratePDF(outputPath)
-		l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss),":","Number",i)
+		l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss), "Number", i)
 		bar.Add(1)
 
 		// time.Sleep(1000 * time.Millisecond)
 	}
-	
-	fmt.Println("")
+
+	fmt.Println("complete !")
+	l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss),"complete !")
 
 }
-
-
