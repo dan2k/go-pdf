05bffce8c3973f9a05ee3ba8045bb6b19202cb83
dan2k <sangtong@gmail.com>
2022-09-02 03:28:41 +0000

    test log

--- go.mod
+++ go.mod
@@ -3,3 +3,21 @@ module go-pdf
 go 1.18
 
 require github.com/SebastiaanKlippert/go-wkhtmltopdf v1.7.2
+
+require (
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.3 // indirect
+	github.com/rivo/uniseg v0.3.4 // indirect
+	github.com/schollz/progressbar/v3 v3.10.0 // indirect
+	github.com/xuri/efp v0.0.0-20220603152613-6918739fd470 // indirect
+	github.com/xuri/excelize/v2 v2.6.1 // indirect
+	github.com/xuri/nfp v0.0.0-20220409054826-5e722a1d9e22 // indirect
+	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8 // indirect
+	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	u "go-pdf/pdfGenerator"
 	"fmt"
+	u "go-pdf/pdfGenerator"
+	"github.com/schollz/progressbar/v3"
+	"time"
+	"github.com/xuri/excelize/v2"
+	"log"
+    "os"
 )
-
+var (
+    outfile, _ = os.Create("my.log") // update path for your needs
+    l      = log.New(outfile, "", 0)
+)
+const (
+    DDMMYYYYhhmmss = "2006-01-02 15:04:05"
+)
 func main() {
 
 	r := u.NewRequestPdf("")
@@ -29,11 +40,88 @@ func main() {
 		Contact:     "Maria Anders",
 		Country:     "Germany",
 	}
-    
+
 	if err := r.ParseTemplate(templatePath, templateData); err == nil {
-		ok, _ := r.GeneratePDF(outputPath)
-		fmt.Println(ok, "pdf generated successfully")
+		// ok, _ := r.GeneratePDF(outputPath)
+		// fmt.Println(ok, "pdf generated successfully")
 	} else {
 		fmt.Println(err)
 	}
+
+	/*
+	bar := progressbar.NewOptions(
+		100, 
+		progressbar.OptionSetPredictTime(true),
+		progressbar.OptionShowElapsedTimeOnFinish(),
+		progressbar.OptionShowCount(),
+		progressbar.OptionSetDescription("กำลังประมวลผล..."),
+	)
+	
+	for i := 0; i < 100; i++ {
+		bar.Add(1)
+		time.Sleep(40 * time.Millisecond)
+	}
+	
+	fmt.Println("")
+	*/
+	//######################################################
+	var xlsFile ="test.xlsx"
+	fmt.Printf("Open %s \n",xlsFile)
+	f, err := excelize.OpenFile(xlsFile)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+	fmt.Println("Open file complete!")
+    defer func() {
+        // Close the spreadsheet.
+        if err := f.Close(); err != nil {
+            fmt.Println(err)
+        }
+    }()
+    // Get value from cell by given worksheet name and axis.
+	
+	rows, err := f.GetRows("Sheet1")
+
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+	fmt.Printf("Total %d rows \n",len(rows)-1)
+	bar := progressbar.NewOptions(
+		len(rows)-1, 
+		progressbar.OptionEnableColorCodes(true),
+		progressbar.OptionSetPredictTime(true),
+		progressbar.OptionShowElapsedTimeOnFinish(),
+		progressbar.OptionShowCount(),
+		progressbar.OptionSetDescription("กำลังประมวลผล..."),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "[green]▓[reset]",
+        	SaucerHead:    "[green]▶[reset]",
+			SaucerPadding: "",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}),
+	)
+	/*
+    for _, row := range rows {
+        for _, colCell := range row {
+            fmt.Print(colCell, "\t")
+        }
+        fmt.Println()
+    }
+	*/
+	
+	for i := 1; i < len(rows); i++ {
+		r.GeneratePDF(outputPath)
+		l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss),":","Number",i)
+		bar.Add(1)
+
+		// time.Sleep(1000 * time.Millisecond)
+	}
+	
+	fmt.Println("")
+
 }
+
+
