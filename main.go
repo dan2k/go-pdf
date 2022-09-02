8d74689cbd18b49c70ce0b77fd843533f590ce28
dan2k <sangtong@gmail.com>
2022-09-02 07:46:37 +0000

    qr

--- main.go
+++ main.go
@@ -120,5 +120,5 @@ func main() {
 
 }
 func AddPath(f string) string {
-	return fmt.Sprintf("file://%s/%s", filepath.Dir(os.Args[0]), f)
+	return fmt.Sprintf("file:///%s/%s", filepath.Dir(os.Args[0]), f)
 }
