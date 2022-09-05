ac38bea3add522bb2b9da2b55f9f8afb19680596
dan2k <sangtong@gmail.com>
2022-09-05 14:20:56 +0000

    test gen

--- log/log.go
+++ log/log.go
@@ -4,19 +4,15 @@ import(
 	"log"
 	"os"
 	"github.com/joho/godotenv"
-	"time"
-)
-const (
-	DDMMYYYYhhmmss = "20060102-15:04:05"
 )
 var L *log.Logger
 var Envs map[string]string 
- func  New() {
+ func  New(runtime string) {
 	envs, err := godotenv.Read(".env")
     if err != nil {
         log.Fatal("Error loading .env file")
     }
-	outfile, _ := os.Create(envs["LOGFILE"]+"-"+time.Now().UTC().Format(DDMMYYYYhhmmss)) // update path for your needs
+	outfile, _ := os.Create(envs["LOGFILE"]+"-"+runtime) // update path for your needs
 	Envs=envs
 	L          = log.New(outfile, "", 0)
 } 
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
 	log "go-pdf/log"
 	u "go-pdf/pdfGenerator"
+	"os"
+	"strings"
 	"time"
+
 	"github.com/schollz/progressbar/v3"
 	qrcode "github.com/skip2/go-qrcode"
 	"github.com/xuri/excelize/v2"
@@ -13,12 +16,15 @@ import (
 
 const (
 	DDMMYYYYhhmmss = "2006-01-02 15:04:05"
+	DDMMYYYYhhmmss2 = "20060102-15:04:05"
 )
 
 var envs map[string]string
-
+var runtime =time.Now().UTC().Format(DDMMYYYYhhmmss2)
 func main() {
+	
 	var startRow,endRow int 
+
 	// endRow :=flag.Int("end",0,"an int ")
 	// startRow := flag.Int("start", 0, "an int")
 	// fmt.Println(*startRow,*endRow);
@@ -27,19 +33,16 @@ func main() {
 	flag.Parse()
 
 	fmt.Println(startRow,endRow);
-	log.New()
+	log.New(runtime)
 	envs :=log.Envs
 	l :=log.L
-	err := qrcode.WriteFile("https://example.org", qrcode.Medium, 256, envs["QRCODE"])
-	if err != nil {
-		fmt.Println(err)
-	}
+	
 	r := u.NewRequestPdf("",l,envs)
 
 	//html template path
 	templatePath := envs["TEMPDIR"] + "/" + envs["TEMPFILE"]
 	//path for download pdf
-	outputPath := envs["STORAGE"] + "/" + envs["PDFFILE"]
+	outputPath := envs["STORAGE"] 
 
 	//html template data
 	type templateData struct {
@@ -50,23 +53,12 @@ func main() {
 		Country     string
 		Labels      []string
 		Data        []int
+		Qrcode      string 
+		Pid         string
 	}
-	tmp := templateData{
-		Title:       "HTML to PDF generator",
-		Description: "This is the simple HTML to PDF file.",
-		Company:     "Jhon Lewis",
-		Contact:     "Maria Anders",
-		Country:     "Germany",
-		Labels: 	 []string{"Red", "Blue", "Yellow", "Green", "Purple", "Orange"},
-		Data:        []int{12, 19, 3, 5, 2, 3},
-	}
-	if err := r.ParseTemplate(templatePath, tmp); err == nil {
-		// ok, _ := r.GeneratePDF(outputPath)
-		// fmt.Println(ok, "pdf generated successfully")
-	} else {
-		fmt.Println(err)
-		l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss), err)
-	}
+	
+
+	
 
 	//######################################################
 	var xlsFile = envs["EXCELFILE"]
@@ -80,13 +72,7 @@ func main() {
 	}
 	fmt.Println("Open file complete!")
 	l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss), "Open file complete!")
-	defer func() {
-		// Close the spreadsheet.
-		if err := f.Close(); err != nil {
-			fmt.Println(err)
-			l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss), err)
-		}
-	}()
+	
 	// Get value from cell by given worksheet name and axis.
 
 	rows, err := f.GetRows("Sheet1")
@@ -119,14 +105,54 @@ func main() {
 			l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss), "complete !")
 		}),
 	)
+	if err := os.Mkdir(outputPath+"/"+runtime, os.ModePerm); err != nil {
+		l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss), err)
+        l.Fatal(err)
+    }
+	qrfile :=envs["QRCODE"]+"/qr-"+runtime+".png"
 	for i := 1; i < len(rows); i++ {
-		r.GeneratePDF(outputPath)
-		l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss), "Number", i)
+		pid:=rows[i][0]
+		t:=strings.Split(qrfile, "/")
+		t2:=strings.Join(t[1:int(len(t))],"/")
+		err := qrcode.WriteFile(pid, qrcode.Medium, 256, qrfile)
+		if err != nil {
+			fmt.Println(err)
+		}
+		tmp := templateData{
+			Title:       "HTML to PDF generator",
+			Description: "This is the simple HTML to PDF file.",
+			Company:     "Jhon Lewis",
+			Contact:     "Maria Anders",
+			Country:     "Germany",
+			Labels: 	 []string{"Red", "Blue", "Yellow", "Green", "Purple", "Orange"},
+			Data:        []int{12, 19, 3, 5, 2, 3},
+			Qrcode:      t2,
+			Pid:		 pid,
+		}
+		if err := r.ParseTemplate(templatePath, tmp); err == nil {
+			r.GeneratePDF(outputPath+"/"+runtime+"/"+pid+".pdf")
+			l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss), "PID", pid)
+		} else {
+			fmt.Println(err)
+			l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss), err)
+		}
 		bar.Add(1)
-
 		// time.Sleep(1000 * time.Millisecond)
 	}
-	
+	defer func() {
+		// Close the spreadsheet.
+		if err := f.Close(); err != nil {
+			fmt.Println(err)
+			l.Println(time.Now().UTC().Format(DDMMYYYYhhmmss), err)
+		}
+		if _, err := os.Stat(qrfile); err ==nil {
+			dir, err := os.Getwd()
+			if err == nil {
+				os.Remove(dir+"/"+qrfile)
+			}
+		}
+
+	}()
 
 	
 
